3f9b36f4550d9224b5c18dcd133cdd3e39630408
harishab123 <harishmca2020@gmail.com>
2025-05-22 11:07:49 -0400

    Create Book.py

---
+++ Book.py
@@ -0,0 +1,10 @@
+class Book:
+    def __init__(self, title, author, isbn):
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+        self.available = True
+        self.reserved_by = None
+
+    def __str__(self):
+        return f"'{self.title}' by {self.author} (ISBN: {self.isbn})"
